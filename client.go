ccd26e72e38c78817030f7ef16f79a64c5b933aa
Konstantin Ivanin <ivanin.k@gmail.com>
2021-05-29 12:43:36 +0300

    Add client example

---
+++ client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+func main() {
+	data := url.Values{
+		"name":    {"name"},
+		"host":    {"localhost"},
+		"port":    {"5672"},
+		"user":    {"guest"},
+		"pass":    {"guest"},
+		"duarble": {"true"},
+		"usessl":  {"false"},
+	}
+
+	resp, err := http.PostForm("http://localhost/api/v1/create_connection", data)
+	if err != nil {
+		fmt.Println(resp)
+	}
+
+	data = url.Values{
+		"queue_id": {"1"},
+		"body":     {"my text message"},
+	}
+
+	for i := 0; i < 1000; i++ {
+		resp, err := http.PostForm("http://localhost/api/v1/send_message", data)
+		if err != nil {
+			log.Println("Error!")
+		}
+
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		fmt.Println(string(body))
+	}
+}
